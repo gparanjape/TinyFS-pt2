06198c727e5477a515fcc40495b95eee95736eb3
Gautam Paranjape <paranjape_gautam@yahoo.com>
2018-04-03 21:52:18 -0700

    Initial commit

---
+++ src/com/chunkserver/ChunkServer.java
@@ -0,0 +1,100 @@
+package com.chunkserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+import java.net.*;
+//import java.util.Arrays;
+
+import com.interfaces.ChunkServerInterface;
+
+/**
+ * implementation of interfaces at the chunkserver side
+ * @author Shahram Ghandeharizadeh
+ *
+ */
+
+public class ChunkServer implements ChunkServerInterface {
+	final static String filePath = "C:\\Users\\Gautam\\Desktop\\TinyFS-pt2\\csci485\\";
+	public static long counter;
+	private static boolean connected;
+	
+	/**
+	 * Initialize the chunk server
+	 */
+	public ChunkServer(){
+		File dir = new File(filePath);
+		File[] fs = dir.listFiles();
+
+		if(fs.length == 0){
+			counter = 0;
+		}else{
+			long[] cntrs = new long[fs.length];
+			for (int j=0; j < cntrs.length; j++)
+				cntrs[j] = Long.valueOf( fs[j].getName() ); 
+			
+			Arrays.sort(cntrs);
+			counter = cntrs[cntrs.length - 1];
+		}
+		ServerSocket ss = null;
+		connected = false;
+//		try {
+//			//ss = new ServerSocket(port);
+		//}
+	}
+	
+	/**
+	 * Each chunk is corresponding to a file.
+	 * Return the chunk handle of the last chunk in the file.
+	 */
+	public String initializeChunk() {
+		counter++;
+		return String.valueOf(counter);
+	}
+	
+	/**
+	 * Write the byte array to the chunk at the offset
+	 * The byte array size should be no greater than 4KB
+	 */
+	public boolean putChunk(String ChunkHandle, byte[] payload, int offset) {
+		try {
+			//If the file corresponding to ChunkHandle does not exist then create it before writing into it
+			RandomAccessFile raf = new RandomAccessFile(filePath + ChunkHandle, "rw");
+			raf.seek(offset);
+			raf.write(payload, 0, payload.length);
+			raf.close();
+			return true;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * read the chunk at the specific offset
+	 */
+	public byte[] getChunk(String ChunkHandle, int offset, int NumberOfBytes) {
+		try {
+			//If the file for the chunk does not exist the return null
+			boolean exists = (new File(filePath + ChunkHandle)).exists();
+			if (exists == false) return null;
+			
+			//File for the chunk exists then go ahead and read it
+			byte[] data = new byte[NumberOfBytes];
+			RandomAccessFile raf = new RandomAccessFile(filePath + ChunkHandle, "rw");
+			raf.seek(offset);
+			raf.read(data, 0, NumberOfBytes);
+			raf.close();
+			return data;
+		} catch (IOException ex){
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+public static void main(String[] args){
+	System.out.println("done");
+}
+
+}
